98124a931d39a2b8a84ece9697a19b447ff45ccf
Jake Miller <102702789+doulosjakem@users.noreply.github.com>
2023-10-18 21:47:16 +0000

    updated home.php

--- index.php
+++ index.php
@@ -24,37 +24,8 @@
     <!--Navigation-->
   <div>
     <?php include './modules/header.php'; ?>
-  </div>
-    <!--End Navigation-->
-    <!--Carousel-->
-    <div class="container text-center">
-        <div class="row">
-          <div class="col-md-12">
-          </div>
-  
-        <div class="col-md-12">
-          <h2>
-            Featured Items
-          </h2>
-          <div>
-            <?php include './modules/home.php'; ?>
-          </div>
-        </div>
-      </div>
-    </div>
-    <!--End carousel-->
-    <!--About Me-->
-    <div class="container text-center">
-        <h2>
-            About The Barn
-        </h2>
-        <p>
-            Lorem ipsum dolor sit amet, consectetur adipisicing elit. Nam quae, accusamus quod ad veniam vero, molestias 
-            sequi possimus adipisci voluptate eos alias animi architecto ullam, 
-            blanditiis harum laborum. Illum, perspiciatis.
-        </p>
-    </div>
-    <!--End About Me-->
+  </div>    
+     <?php include './modules/home.php'; ?>
 </main>
 <footer>
     <?php include './modules/footer.php'; ?>
