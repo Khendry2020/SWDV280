4061c42ad2c1b95c6f0227464cd6bda39038d8b3
Jake Miller <102702789+doulosjakem@users.noreply.github.com>
2023-10-18 20:10:30 +0000

    Bootstrap.js and jquery set up

--- modules/head.php
+++ modules/head.php
@@ -7,7 +7,8 @@
     
     <!-- Bootstrap 5 CDN / Bootstrap bundle with popper / Bootstrap icons-->
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/css/bootstrap.min.css" rel="stylesheet">
-    
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/js/bootstrap.bundle.min.js" integrity="sha384-C6RzsynM9kWDrMNeT87bh95OGNyZPhcTNXj1NW7RuBCsyN/o0jlpcV8Qyq46cDfL" crossorigin="anonymous"></script>
+    <script src="https://code.jquery.com/jquery-3.7.1.min.js"></script>
     
     <!-- Font Icons link here-->
 
@@ -16,6 +17,7 @@
     <!-- style sheet css here -->
     
     <!-- JavaScript here-->
+    <script src="./scripts/script.js"></script>
     
 
 </head>
