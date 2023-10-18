60960cf6a1359dea6b7a47527c4525ee933c6b5c
nayezapata <102701740+nayezapata@users.noreply.github.com>
2023-10-18 14:02:11 -0600

    Create head and header php files.

---
+++ modules/head.php
@@ -0,0 +1,21 @@
+<head>
+    <!-- Required meta tags -->
+	<meta charset="utf-8">
+	<meta name="viewport" content="width=device-width, initial-scale=1">
+    <!-- <meta name="author" content=" "> -->  
+    <!-- <title>Italian Restaurant</title> -->
+    
+    <!-- Bootstrap 5 CDN / Bootstrap bundle with popper / Bootstrap icons-->
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/css/bootstrap.min.css" rel="stylesheet">
+    
+    
+    <!-- Font Icons link here-->
+
+
+
+    <!-- style sheet css here -->
+    
+    <!-- JavaScript here-->
+    
+
+</head>
---
+++ modules/header.php
@@ -0,0 +1,19 @@
+<header>
+	<nav class="navbar navbar-expand-sm bg-light">
+		<div class="container-fluid">
+			<a class="navbar-brand " href="#"><img src=" " alt="" style="width:40px;" class="rounded-pill"> Furniture name </a>
+
+			<button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarResponsive">
+				<span class="navbar-toggler-icon"></span>
+			</button>
+
+			<div class="collapse navbar-collapse" id="navbarResponsive">
+				<ul class="navbar-nav">
+					<li class="nav-item"><a class="nav-link" href="#">Home</a></li>
+					<li class="nav-item"><a class="nav-link" href="#">Galery</a></li>
+				</ul>
+			</div>
+			
+		</div>
+	</nav> 
+</header>
