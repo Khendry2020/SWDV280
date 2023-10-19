05f9fd80599e2d3ec2e3a0d326239b15ee56ec53
Nayeli Zapata Reyes <nayes.nzr@gmail.com>
2023-10-19 14:09:57 -0600

    Completed head and header.

--- modules/head.php
+++ modules/head.php
@@ -2,10 +2,10 @@
     <!-- Required meta tags -->
 	<meta charset="utf-8">
 	<meta name="viewport" content="width=device-width, initial-scale=1">
-    <!-- <meta name="author" content=" "> -->  
-    <!-- <title>Italian Restaurant</title> -->
+    <meta name="author" content="SWDV280">
+    <title>Scott's Furniture Barn</title> 
     
-    <!-- Bootstrap 5 CDN / Bootstrap bundle with popper / Bootstrap icons-->
+    <!-- Bootstrap 5 CDN-->
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/css/bootstrap.min.css" rel="stylesheet">
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/js/bootstrap.bundle.min.js" integrity="sha384-C6RzsynM9kWDrMNeT87bh95OGNyZPhcTNXj1NW7RuBCsyN/o0jlpcV8Qyq46cDfL" crossorigin="anonymous"></script>
     <script src="https://code.jquery.com/jquery-3.7.1.min.js"></script>
@@ -16,11 +16,14 @@
     <link rel="icon" type="image/png" sizes="16x16" href="./favicon/favicon-16x16.png">
     <link rel="manifest" href="./favicon/site.webmanifest">
     
-    <!-- Font Icons link here-->
+    <!-- Google Fonts Material Symbols-->
+    <link href="https://fonts.googleapis.com/css2?family=Material+Symbols+Outlined" rel="stylesheet" />
+    <link rel="stylesheet" href="https://fonts.googleapis.com/css2?family=Material+Symbols+Outlined:opsz,wght,FILL,GRAD@24,400,0,0" />
 
 
 
     <!-- style sheet css here -->
+    <link rel="stylesheet"href="./styles/styles.css">
     
     <!-- JavaScript here-->
     <script src="./scripts/script.js"></script>
--- modules/header.php
+++ modules/header.php
@@ -1,19 +1,28 @@
 <header>
-	<nav class="navbar navbar-expand-sm bg-light">
-		<div class="container-fluid">
-			<a class="navbar-brand " href="#"><img src=" " alt="" style="width:40px;" class="rounded-pill"> Furniture name </a>
+	<nav class="navbar navbar-expand-sm bg-dark navbar-dark justify-content-between">
+		<div class="container-fluid py-2">
+			<a class="navbar-brand " href="#"><img src="../favicon/android-chrome-192x192.png" alt="" style="width:40px;" class="rounded-pill"> Scott's Furniture Barn </a>
 
 			<button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarResponsive">
 				<span class="navbar-toggler-icon"></span>
 			</button>
 
-			<div class="collapse navbar-collapse" id="navbarResponsive">
+			<div class="collapse navbar-collapse justify-content-center" id="navbarResponsive">
 				<ul class="navbar-nav">
-					<li class="nav-item"><a class="nav-link" href="#">Home</a></li>
-					<li class="nav-item"><a class="nav-link" href="#">Gallery</a></li>
+					<li class="nav-item pe-4"><a class="nav-link" href="#">Home</a></li>
+					<li class="nav-item pe-4"><a class="nav-link" href="#">Gallery</a></li>
+					<li class="nav-item pe-4"><a class="nav-link" href="#">Contact</a></li>
 				</ul>
 			</div>
+
+			<a href="#"> <!-- to reserved items page ? -->
+				<div class="position-relative bg-light text-dark p-2 rounded">
+					<span class="material-symbols-outlined">shopping_cart</span>
+					<div id="cartAmount" class="position-absolute bg-dark text-light ps-1 pe-1 rounded">0</div> 
+				</div>
+        	</a>
 			
 		</div>
+
 	</nav> 
 </header>
