745650bd1be524e7f8af91b482fb616bde89e792
Jake Miller <doulosjakem@gmail.com>
2023-10-20 13:45:10 -0600

    test

--- modules/home.php
+++ modules/home.php
@@ -1,7 +1,7 @@
 <!-- This is the mobile carousel for featured items, should be invisible on medium and up,
      This will need to be looped with PHP to create items when we have DB set up.  -->
 <div class="container">
-    <h2 class="text-center">Featured Items</h2>
+    <h2 class="text-center">Featured Items and noodles</h2>
 test
     <div class="container-fluid visible invisible-md"> 
         <div id="mobileFeatured" class="carousel slide " data-bs-ride="carousel">
