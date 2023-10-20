fb3c7edfbc88045af7a25b1ea5d135064da3f684
Khendry2020 <khendry97@outlook.com>
2023-10-20 13:23:50 -0600

    modified:   index.php

--- index.php
+++ index.php
@@ -1,35 +1,37 @@
 <!DOCTYPE html>
-<?php include'./modules/head.php'; ?>
+<?php include './modules/head.php'; ?>
+
 <body>
-    <!-- ======= Hero Section =======  -->
+  <!-- ======= Hero Section =======  -->
   <section id="hero" class="hero">
     <div class="container text-center">
       <div class="row">
         <div class="col-md-12">
         </div>
 
-      <div class="col-md-12">
-        <h1>
-          Scott's Furniture Barn
-        </h1>
+        <div class="col-md-12">
+          <h1>
+            Scott's Furniture Barn
+          </h1>
 
-        <p class="tagline">
-          Lorem ipsum dolor, sit amet consectetur adipisicing elit. 
-        </p>
+          <p class="tagline">
+            Lorem ipsum dolor, sit amet consectetur adipisicing elit.
+          </p>
+        </div>
       </div>
-    </div>
 
   </section><!-- End Hero -->
-<main>
+  <main>
     <!--Navigation-->
-  <div>
-    <?php include './modules/header.php'; ?>
-  </div>    
-     <?php include './modules/home.php'; ?>
-</main>
-<footer>
+    <div>
+      <?php include './modules/header.php'; ?>
+    </div>
+    <?php include './modules/home.php'; ?>
+  </main>
+  <footer>
     <?php include './modules/footer.php'; ?>
-</footer>
+  </footer>
 
 </body>
-</html>
+
+</html>asdfasdfa
