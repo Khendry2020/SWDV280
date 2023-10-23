07e1623dcbc70db89e18771e34fc85deafb91cd9
Jake Miller <doulosjakem@gmail.com>
2023-10-23 14:56:12 -0600

    rounded about.php

--- modules/about.php
+++ modules/about.php
@@ -1,6 +1,6 @@
 <div class="container pb-5">
     <h2 class="text-center pt-5">About Scott's Funiture Barn</h2>
-    <div class="container row p-3 color-orange color-light">
+    <div class="container row p-3 color-orange color-light rounded">
         <div class="col-md-6 col-12">
             <p>Lorem ipsum, dolor sit amet consectetur adipisicing elit. Perspiciatis error, autem perferendis numquam nulla quis vitae quia! Dolor dolore alias itaque, deserunt quia enim nihil quidem, cupiditate possimus repellendus obcaecati. Lorem ipsum dolor sit amet consectetur adipisicing elit. Ad, tenetur asperiores dicta cum consequuntur velit quis dignissimos, possimus quod assumenda sunt est vero ea id beatae? Eaque dolores fuga dolorem.</p>
         </div>
