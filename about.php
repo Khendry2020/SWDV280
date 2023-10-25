8797d0fc64a7e3b73640391497df183335fd7834
Jake Miller <doulosjakem@gmail.com>
2023-10-25 16:15:21 -0600

    updates to logo usage throughout site

--- about.php
+++ about.php
@@ -7,19 +7,7 @@
       <div>
         <?php include './modules/header.php'; ?>
       </div>    
-      <div class="container pb-5">
-        <h2 class="text-center pt-5">About Scott's Funiture Barn</h2>
-          <div class="container row p-3 color-orange color-light rounded">
-            <div class="col-md-6 col-12">
-                <p>Lorem ipsum, dolor sit amet consectetur adipisicing elit. Perspiciatis error, autem perferendis numquam nulla quis vitae quia! Dolor dolore alias itaque, deserunt quia enim nihil quidem, cupiditate possimus repellendus obcaecati. Lorem ipsum dolor sit amet consectetur adipisicing elit. Ad, tenetur asperiores dicta cum consequuntur velit quis dignissimos, possimus quod assumenda sunt est vero ea id beatae? Eaque dolores fuga dolorem.</p>
-            </div>
-          <div class="col-md-6 col-12"><img src="./favicon/android-chrome-512x512.png" class="img-fluid rounded p-1"></div>
-          <div class="col-md-6 col-12"><img src="./favicon/android-chrome-512x512.png" class="img-fluid rounded p-1"></div>
-          <div class="col-md-6 col-12">
-            <p>Lorem ipsum, dolor sit amet consectetur adipisicing elit. Perspiciatis error, autem perferendis numquam nulla quis vitae quia! Dolor dolore alias itaque, deserunt quia enim nihil quidem, cupiditate possimus repellendus obcaecati.</p>
-          </div>
-        </div>
-      </div>
+      <?php include './modules/about.php'; ?>
     </main>
     <footer>
         <?php include './modules/footer.php'; ?>
