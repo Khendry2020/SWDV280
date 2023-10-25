c122b299888164d907be2bcb496cc9eb48fd3152
CyrilSoltau <113400067+CyrilSoltau@users.noreply.github.com>
2023-10-25 15:12:04 -0600

    Adding donation query that select from the Users and donation tables

---
+++ admin/model/donation_db.php
@@ -0,0 +1,16 @@
+function donationFuniture(){
+    global $db;
+    try{
+    $query = 'SELECT Name, Phone, Img, Description From Users Join  	
+              DonatedFurniture where Users.UserId =  DonatedFurniture.UserId'
+         $statement = $dba->prepare($query);
+         $statement->execute();
+         $result = $statement->fetchAll();
+         $statement->closeCursor();
+         return $result;
+    }
+     catch(PDOException $e){
+         $error_message = $e->getMessage();
+             display_db_error($error_message);
+     }         
+}
