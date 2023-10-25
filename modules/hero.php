7e4e01980ec474c3768274770381426567df6541
Jake Miller <doulosjakem@gmail.com>
2023-10-25 14:44:20 -0600

    hero redesigned

--- modules/hero.php
+++ modules/hero.php
@@ -1,17 +1,4 @@
 <!-- ======= Hero Section =======  -->
-<section id="hero" class="hero">
-      <div class="container text-center">
-        <div class="row">
-          <div class="col-md-12">
-          </div>
-          <div class="col-md-12">
-            <h1>
-              Scott's Furniture Barn
-            </h1>
-            <p class="tagline">
-              Lorem ipsum dolor, sit amet consectetur adipisicing elit. 
-            </p>
-          </div>
-        </div>
-      </div>
-    </section><!-- End Hero -->
+<section id="hero" class="hero text-center bg-dark">      
+  <img src="./images/scottslogo.png" alt="" class="img-fluid">
+</section><!-- End Hero -->
