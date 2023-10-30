1ecc4348fff60f8a4192bc153276140218677cab
Brandon Swan <arcaneblunder@gmail.com>
2023-10-30 15:29:06 -0600

    Added create_account test

--- create_account_test.php
+++ create_account_test.php
@@ -6,30 +6,41 @@ include('models/users.php');
 if (isset($_POST['create'])) {
     // Trim inputs
     $_POST['name'] = trim($_POST['name']);
-    $_POST['description'] = trim($_POST['description']);
-    $_POST['price'] = trim($_POST['price']);
-    $_POST['category'] = trim($_POST['category']);
+    $_POST['email'] = trim($_POST['email']);
+    $_POST['phone'] = trim($_POST['phone']);
+    $_POST['street'] = trim($_POST['street']);
+    $_POST['city'] = trim($_POST['city']);
+    $_POST['state'] = trim($_POST['state']);
+    $_POST['zip'] = trim($_POST['zip']);
 
     $name = filter_input(INPUT_POST, 'name');
-    $description = filter_input(INPUT_POST, 'description');
-    $price = filter_input(INPUT_POST, 'price', FILTER_VALIDATE_FLOAT);
-    $category_id = filter_input(INPUT_POST, 'category', FILTER_VALIDATE_INT);
+    $email = filter_input(INPUT_POST, 'email', FILTER_VALIDATE_EMAIL);
+    $phone = filter_input(INPUT_POST, 'phone');
+    $street = filter_input(INPUT_POST, 'street');
+    $city = filter_input(INPUT_POST, 'city');
+    $state = filter_input(INPUT_POST, 'state');
+    $zip = filter_input(INPUT_POST, 'zip');
 
-    if ($name == NULL || $description == NULL ||
-            $price == FALSE || $category_id == NULL) {            
-        $error = 'Invalid product data. Check all fields and try again.';
-        include('../../errors/error.php');
-    } else if ($price <= 0) {
-        $error = 'Price of item cannot be 0 or less than 0.';
-        include('../../errors/error.php');
+    if ($name == NULL || $email == NULL || $phone == FALSE || $street == NULL || $city == NULL || $state == NULL || $zip == NULL) {            
+        $error = 'Invalid user data. Check all fields and try again.';
+        include('errors/error.php');
     } else {
-        // Add item to database
-        add_item($category_id, $name, $description, $price, $img);
 
-        $_POST = [];
+        $email_check = check_email($email);
+        var_dump($email_check);
+        if($email_check != NULL || $email_check != FALSE || $email_check != 0) {
+            $error = 'This email address is already in use. Please try another email address.';
+            include('errors/error.php');
+        } else {
+            // Add item to database
+            add_address($street, $city, $state, $zip);
+            $last_id = $db->lastInsertId();
+            add_user($name, $email, $phone, $last_id);
+            $_POST = [];
 
-        $_SESSION['Status Message'] = 'Item added successfully.';
-        header("Location: ../products.php");
+            $_SESSION['Status Message'] = 'Your account has been successfully created.';
+            header("Location: account.php");
+        }
     }
 }
 
@@ -49,8 +60,31 @@ if (isset($_POST['create'])) {
             <label for="name" class="form-label">Name</label>
             <input type="text" class="form-control" id="name" name="name">
         </div>
-
-        <button type="submit" class="btn btn-primary mt-3" name="add">Submit</button>
+        <div class="mb-3">
+            <label for="email" class="form-label">Email</label>
+            <input type="text" class="form-control" id="email" name="email">
+        </div>
+        <div class="mb-3">
+            <label for="phone" class="form-label">Phone</label>
+            <input type="text" class="form-control" id="phone" name="phone">
+        </div>
+        <div class="mb-3">
+            <label for="street" class="form-label">Street Address</label>
+            <input type="text" class="form-control" id="street" name="street">
+        </div>
+        <div class="mb-3">
+            <label for="city" class="form-label">City</label>
+            <input type="text" class="form-control" id="city" name="city">
+        </div>
+        <div class="mb-3">
+            <label for="state" class="form-label">State</label>
+            <input type="text" class="form-control" id="state" name="state">
+        </div>
+        <div class="mb-3">
+            <label for="zip" class="form-label">Zip</label>
+            <input type="text" class="form-control" id="zip" name="zip">
+        </div>
+        <button type="submit" class="btn btn-primary mt-3" name="create">Create Account</button>
     </form>
   </main>
   <footer>
--- models/users.php
+++ models/users.php
@@ -15,19 +15,19 @@ function get_user($user_id) {
     }
 }
 
-function add_user($name) {
+function add_user($name, $email, $phone, $address_id) {
     global $db;
-    $query = 'INSERT INTO users (CategoryType)
-              VALUES (:name)';
+    $query = 'INSERT INTO users (Name, Email, Phone, AddressId)
+              VALUES (:name, :email, :phone, :address_id)';
     try {
         $statement = $db->prepare($query);
         $statement->bindValue(':name', $name);
+        $statement->bindValue(':email', $email);
+        $statement->bindValue(':phone', $phone);
+        $statement->bindValue(':address_id', $address_id);
         $statement->execute();
         $statement->closeCursor();
 
-        // Get the last product ID that was automatically generated
-        $category_id = $db->lastInsertId();
-        return $category_id;
     } catch (PDOException $e) {
         $error_message = $e->getMessage();
         display_db_error($error_message);
@@ -51,19 +51,37 @@ function update_user($cat_id, $name) {
         display_db_error($error_message);
     }
 }
-// Add Address to database
-// will need get last ID
-function add_address() {
+function check_email($email) {
     global $db;
-    $query = '
-        UPDATE categories
-        SET CategoryType = :name
-        WHERE CategoryId = :cat_id';
+    $query = 'SELECT Email FROM users
+              WHERE Email = :email';
     try {
         $statement = $db->prepare($query);
-        $statement->bindValue(':name', $name);
-        $statement->bindValue(':cat_id', $cat_id);
+        $statement->bindValue(':email', $email);
         $statement->execute();
+        $result = $statement->fetch();
+        $statement->closeCursor();
+        return $result;
+    } catch (PDOException $e) {
+        display_db_error($e->getMessage());
+    }
+}
+// Add Address to database
+// will need get last ID
+function add_address($street, $city, $state, $zip) {
+    global $db;
+    $query = 'INSERT INTO address
+                 (Streetaddress, city, State, Zip)
+              VALUES
+                 (:street, :city, :state, :zip)';
+    try {
+        $statement = $db->prepare($query);
+        $statement->bindValue(':street', $street);
+        $statement->bindValue(':city', $city);
+        $statement->bindValue(':state', $state);
+        $statement->bindValue(':zip', $zip);
+        $statement->execute();
+        $last_id = $db->lastInsertId();
         $statement->closeCursor();
     } catch (PDOException $e) {
         $error_message = $e->getMessage();
