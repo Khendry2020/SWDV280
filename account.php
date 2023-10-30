41bf4944f261cd202c7a1abbf506054636a4f83a
Jake Miller <doulosjakem@gmail.com>
2023-10-30 13:58:50 -0600

    account page under construction init

---
+++ account.php
@@ -0,0 +1,21 @@
+<?php
+session_start();
+?>
+<!DOCTYPE html>
+<?php include './modules/head.php'; ?>
+
+<body>
+  <?php include './modules/hero.php'; ?>
+  <main>
+    <!--Navigation-->
+    <div>
+      <?php include './modules/header.php'; ?>
+    </div>
+    <?php include './modules/accountBody.php'; ?>
+  </main>
+  <footer>
+    <?php include './modules/footer.php'; ?>
+  </footer>
+</body>
+
+</html>
