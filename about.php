848ffeeec0d78444c6f83c5418cd785c0f230f14
Khendry2020 <khendry97@outlook.com>
2023-11-03 14:04:38 -0600

    modified:   about.php

--- about.php
+++ about.php
@@ -1,16 +1,18 @@
 <!DOCTYPE html>
-  <?php include'./modules/head.php'; ?>
-  <body>
-     <?php include './modules/hero.php'; ?>
-    <main>
-        <!--Navigation-->
-      <div>
-        <?php include './modules/header.php'; ?>
-      </div>    
-      <?php include './modules/aboutBody.php'; ?>
-    </main>
-    <footer>
-        <?php include './modules/footer.php'; ?>
-    </footer>
-  </body>
+<?php include './modules/head.php'; ?>
+
+<body>
+  <?php include './modules/hero.php'; ?>
+  <main>
+    <!--Navigation-->
+    <div>
+      <?php include './modules/header.php'; ?>
+    </div>
+    <?php include './modules/aboutBody.php'; ?>
+  </main>
+  <footer>
+    <?php include './modules/footer.php'; ?>
+  </footer>
+</body>
+
 </html>
