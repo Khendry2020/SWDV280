b173cdecac746faa415a8f3362e9b13a7fa2915a
Brandon Swan <arcaneblunder@gmail.com>
2023-11-03 13:15:02 -0600

    Fixing admin database

--- admin/errors/db_error.php
+++ admin/errors/db_error.php
@@ -1,5 +1,8 @@
+<?php include( $_SERVER['DOCUMENT_ROOT'] . '/swdv280/modules/head.php'); ?>
+<?php include( $_SERVER['DOCUMENT_ROOT'] . '/swdv280/modules/header.php'); ?>
 <section>
     <h1>Database Error</h1>
     <p>A database error occurred.</p>
     <p class="last_paragraph">Error message: <?php echo $error_message; ?></p>
 </section>
+<?php include( $_SERVER['DOCUMENT_ROOT'] . '/swdv280/modules/footer.php'); ?>
--- admin/errors/db_error_connect.php
+++ admin/errors/db_error_connect.php
@@ -1,8 +1,8 @@
-<?php include( $_SERVER['DOCUMENT_ROOT'] .'/swdv280/modules/head.php'); ?>
-<?php include( $_SERVER['DOCUMENT_ROOT'] .'/swdv280/modules/header.php'); ?>
+<?php include( $_SERVER['DOCUMENT_ROOT'] . '/swdv280/modules/head.php'); ?>
+<?php include( $_SERVER['DOCUMENT_ROOT'] . '/swdv280/modules/header.php'); ?>
 <section>
     <h1>Database Error</h1>
     <p>An error occurred connecting to the database.</p>
     <p class="last_paragraph">Error message: <?php echo $error_message; ?></p>
 </section>
-<?php include( $_SERVER['DOCUMENT_ROOT'] .'/swdv280/modules/footer.php'); ?>
+<?php include( $_SERVER['DOCUMENT_ROOT'] . '/swdv280/modules/footer.php'); ?>
