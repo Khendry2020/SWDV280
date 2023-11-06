f61d384daaa0e53060efad8a8705aac17ca8834d
Coleosis <coleosiscarl@gmail.com>
2023-11-06 14:43:20 -0700

    Cards for categories

--- category.php
+++ category.php
@@ -21,8 +21,8 @@ $products = get_items_by_category($category['CategoryId']);
                     if ($category == NULL || $category == 0 || $category === false): ?>
                     <div>The category you are looking for does not exist. Please hit the back button in your browser. If you believe this is an error, please contact support.</div>
                     <?php else: ?>
-                    <a class="link-dark" href="gallery.php"><p class="py-3 my-0 fs-6 fw-light">Back to Categories</p></a> 
-                    <h2>
+                    <h3><a class="link-dark" href="gallery.php"><p class="py-3 my-0 fs-6 fw-light">Back to Categories</p></a></h3> 
+                    <h2 class="text-center">
                         <?php echo $category['CategoryType']; ?>
                     </h2>
                 <?php // Check if the category has products. If not, say no products otherwise show them.
@@ -33,18 +33,22 @@ $products = get_items_by_category($category['CategoryId']);
 
                     <?php else: ?>
 
-                     
-
-                    <ul>
-                        <?php foreach ($products as $product) : ?>
-                        <li>
-                            <a class="text-body" href="product.php?product_id=<?php
-                                    echo $product['ItemId']; ?>">
-                                <?php echo $product['Name']; ?>
-                            </a>
-                        </li>
-                        <?php endforeach; ?>
-                    </ul>
+                     <!--Cards for products listed in 2 columns per row-->
+                    <div class="row row-cols-2 g-3">
+                    <?php foreach ($products as $product) : ?>
+                        <div class="col">                        
+                            <div class="card">                               
+                                    <img class="text-center img-fluid mx-auto mx-lg-0 h-100 col-8 col-sm-6 col-md-4 col-lg-2 my-auto " src="data:image/jpg;charset=utf8;base64,<?php echo base64_encode($product['Img']); ?>" alt="<?php echo $product['Name']; ?>" />
+                                    <div class="card-body">
+                                        <a class="card-title" href="product.php?product_id=<?php
+                                                echo $product['ItemId']; ?>">
+                                            <?php echo $product['Name']; ?>
+                                        </a>                                   
+                                    </div>
+                            </div>                                   
+                        </div>
+                        <?php endforeach; ?>        
+                    </div>
 
                     <?php endif; ?>
 
