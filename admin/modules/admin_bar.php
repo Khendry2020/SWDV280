8319ca6b7ef49b83809a758eceaca3fa98934b27
nayeli <nayes.nzr@gmail.com>
2023-11-06 15:44:09 -0700

    Update admin edit users page.

--- admin/modules/admin_bar.php
+++ admin/modules/admin_bar.php
@@ -13,19 +13,11 @@
 			</button>
 			<div class="collapse navbar-collapse justify-content-center pe-5 " id="navbarResponsive">
 				<ul class="navbar-nav">
-<<<<<<< HEAD
-					<li class="nav-item"><a class="nav-link" href="products.php">Products</a></li>
-					<li class="nav-item"><a class="nav-link" href="categories.php">Categories</a></li>
-					<li class="nav-item"><a class="nav-link" href="admins.php">Users</a></li>
-					<li class="nav-item"><a class="nav-link" href="reports.php">Reports</a></li>
-					<li class="nav-item"><a class="nav-link" href="donations.php">Donations</a></li>
-=======
 					<li class="nav-item"><a class="nav-link" href="/swdv280/admin/products.php">View Products</a></li>
 					<li class="nav-item"><a class="nav-link" href="/swdv280/admin/categories.php">View Categories</a></li>
 					<li class="nav-item"><a class="nav-link" href="/swdv280/admin/admins.php">View Users</a></li>
 					<li class="nav-item"><a class="nav-link" href="/swdv280/admin/reports.php">View Reports</a></li>
 					<li class="nav-item"><a class="nav-link" href="/swdv280/admin/donations.php">View Donations</a></li>
->>>>>>> 45e5bad203cd978fda780a35885a804bd8c23904
 				</ul>
 			</div>
 			<?php
