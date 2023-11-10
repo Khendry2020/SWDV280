d3eee87c1e2b903db3c0fb08536c426fb98ef103
Cyril Soltau <cyrilsoltau@my.cwi.edu>
2023-11-10 13:30:42 -0700

    Added styled bullet points on donation page

--- modules/donationBody.php
+++ modules/donationBody.php
@@ -1,11 +1,12 @@
-<div class="container row p-3 color-orange color-light rounded ">
-    <h1>Donation form download</h1>
+<div class="container row p-3 color-orange color-light rounded mr-5 ">
+    <h1>Donation form download.</h1>
     <p>Download and fillout our form and bring it with you, when you bring your donations or email it to us at  
         <a href="mailto:ScottsFurnitureBarn@gmail.com" class="text-decoration-none">
-                <i class="bi bi-envelope-at">&nbsp;ScottsFurnitureBarn@gmail.com</i>
+                <i class="bi bi-envelope-at ">&nbsp;ScottsFurnitureBarn@gmail.com</i>
         </a>
     </p>
     <ul class="download">
+        
         <h2>Donation Drop-off hours:</h2>
         <li>Monday: 1-5pm</li>
         <li>Tuesday: 1-5pm</li>
@@ -13,10 +14,12 @@
         <li>Turesday: 1-5pm</li>
         <li>Friday: 1-5pm</li>
         <li>Saturday: 2-5pm</li>
-        
+        <button>
+            <a class="button text-decoration-none text-black" href="DonationForm.docx" Download ="ScottsDonationform">Download Donation Form</a>
+        </button>
     </ul>
       
-     <button><a class="button text-decoration-none text-black" href="DonationForm.docx" Download ="ScottsDonationform">Download Donation Form</a></button>
+    
 
 
      <script>
