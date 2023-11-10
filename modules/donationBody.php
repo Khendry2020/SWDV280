d9afb6a9a10a901f4cc4d1ea01117861cf27ebb3
Cyril Soltau <cyrilsoltau@my.cwi.edu>
2023-11-10 15:24:27 -0700

    Added styles for donation page to css style file

--- modules/donationBody.php
+++ modules/donationBody.php
@@ -1,23 +1,19 @@
-
 <style>
-
-
-div > li{
+    .download > li{
 text-decoration: none;
 /* <a href="https://www.flaticon.com/free-icons/farm" title="farm icons">Farm icons created by smashingstocks - Flaticon</a> */
 list-style-image:url('barn.png');
-
 }
 </style>
-<div class="container row p-3 color-orange color-light rounded mr-5 ">
-    <h1>Donation form download.</h1>
-    <p>Download and fillout our form and bring it with you, when you bring your donations or email it to us at  
-        <a href="mailto:ScottsFurnitureBarn@gmail.com" class="text-decoration-none">
+<h1 class="text-center">Donation form download.</h1>
+<div id="download"  class="container color-orange color-light rounded mt-4 download">
+   
+    <p class="text-center">Download and fillout our form and bring it with you, when you bring your donations or email it to us at  
+        <a href="mailto:ScottsFurnitureBarn@gmail.com" class="text-decoration-none color-info">
                 <i class="bi bi-envelope-at ">&nbsp;ScottsFurnitureBarn@gmail.com</i>
         </a>
     </p>
-    <ul class="download">
-        
+    <ul class="download" >
         <h2>Donation Drop-off hours:</h2>
         <li>Monday: 1-5pm</li>
         <li>Tuesday: 1-5pm</li>
@@ -25,11 +21,11 @@ list-style-image:url('barn.png');
         <li>Turesday: 1-5pm</li>
         <li>Friday: 1-5pm</li>
         <li>Saturday: 2-5pm</li>
-        <button>
+        <button class="mt-3 mb-3 rounded">
             <a class="button text-decoration-none text-black" href="DonationForm.docx" Download ="ScottsDonationform">Download Donation Form</a>
         </button>
     </ul>
-      
+</div>     
     
 
 
