6ab9ccd553de11139b78b945c1a9ce5f112da30b
Brandon Swan <arcaneblunder@gmail.com>
2023-11-13 14:10:29 -0700

    Modal added to product.php

--- product.php
+++ product.php
@@ -60,7 +60,7 @@ if ($product != NULL || $product_id != 0 || $product !== false) {
                     
                 <h4 class="fs-4 pb-1"><?php echo $product_name; ?></h4>
                 <div class="">
-                    <img class="img-fluid rounded product-img" src="data:image/jpg;charset=utf8;base64,<?php echo base64_encode($product_image); ?>" alt="<?php echo $product_name; ?>" />
+                    <a data-bs-toggle="modal" href="#productModal" role="button"><img class="img-fluid rounded product-img" src="data:image/jpg;charset=utf8;base64,<?php echo base64_encode($product_image); ?>" alt="<?php echo $product_name; ?>" /></a>
                 </div>
                 <div class="py-2">
                     <p class="fs-6 text-justify"><?php echo $description; ?></p>
@@ -80,7 +80,18 @@ if ($product != NULL || $product_id != 0 || $product !== false) {
             <?php endif; ?>
         </div>
     </main>
+    <div class="modal fade" id="productModal" aria-hidden="true" aria-labelledby="productModal" tabindex="-1">
+        <div class="modal-dialog modal-fullscreen modal-dialog-centered">
+            <div class="modal-content">
+                <div class="modal-header">
+                    <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
+                </div>
+                <div class="modal-body">
+                    <img class="img-fluid rounded d-block ms-auto me-auto" src="data:image/jpg;charset=utf8;base64,<?php echo base64_encode($product_image); ?>" alt="<?php echo $product_name; ?>" />
+                </div>
+            </div>
+        </div>
+    </div>
     <?php include './modules/footer.php'; ?>
 </body>
-
 </html>
