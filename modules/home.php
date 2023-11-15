9bd6e9e7b4611b4e1c674c6ff8ee74fa1b5ae277
nayeli <nayes.nzr@gmail.com>
2023-11-15 14:48:56 -0700

    Updated home.php file to pull featured items imgs from Dbase.

--- modules/home.php
+++ modules/home.php
@@ -1,91 +1,91 @@
-<!-- This is the mobile carousel for featured items, should be invisible on medium and up,
-     This will need to be looped with PHP to create items when we have DB set up.  -->
+ <?php 
+include './models/database.php';
+
+function getFeaturedItems($limit = 4) {
+    global $db;
+    try{
+
+        $query = "SELECT * FROM items WHERE `featured` = 'yes' ORDER BY RAND() LIMIT :limit";
+        $stmt = $db->prepare($query);
+        $stmt->bindParam(':limit', $limit, PDO::PARAM_INT);
+        $stmt->execute();
+        $items = $stmt->fetchAll(PDO::FETCH_ASSOC);
+        return $items;
+    } catch(PDOException $e) {
+        display_db_error($e->getMessage());
+    }
+}
+
+$perfectConditionItems = getFeaturedItems($limit = 4);
+?> 
+
+<!-- This is the mobile carousel for featured items, should be invisible on medium and up-->
 <div class="container">
-    <h2 class="text-center">Featured Items</h2>
+    <h2 class="text-center pt-2">Featured Items</h2>
     <div class="container-fluid" id="mobileCarousel"> 
         <div id="carouselExampleIndicators" class="carousel slide" data-bs-ride="carousel">
             <div class="carousel-indicators">
-                <button type="button" data-bs-target="#carouselExampleIndicators" data-bs-slide-to="0" class="active" aria-current="true" aria-label="Slide 1"></button>
-                <button type="button" data-bs-target="#carouselExampleIndicators" data-bs-slide-to="1" aria-label="Slide 2"></button>
-                <button type="button" data-bs-target="#carouselExampleIndicators" data-bs-slide-to="2" aria-label="Slide 3"></button>
+                <?php foreach ($perfectConditionItems as $key => $item) : ?>
+                    <button type="button" data-bs-target="#carouselExampleIndicators" data-bs-slide-to="<?= $key ?>" <?= $key == 0 ? 'class="active"' : '' ?> aria-label="Slide <?= $key + 1 ?>"></button>
+                <?php endforeach; ?>
             </div>
+
             <div class="carousel-inner">
-                <div class="carousel-item active">
-                <img src="./images/scotts_S.png" class="d-block w-100 img-fluid rounded" alt="...">
-                </div>
-                <div class="carousel-item">
-                <img src="./images/scotts_S.png" class="d-block w-100 img-fluid rounded" alt="...">
-                </div>
-                <div class="carousel-item">
-                <img src="./images/scotts_S.png" class="d-block w-100 img-fluid rounded" alt="...">
-                </div>
+                <?php foreach ($perfectConditionItems as $key => $item) : ?>
+                    <div class="carousel-item <?= $key == 0 ? 'active' : '' ?>">
+                        <?php 
+                            $imgData = base64_encode($item['Img']);
+                            $imgSrc = "data:image/jpeg;base64,{$imgData}";
+                        ?>
+                        <img src="<?= $imgSrc ?>" class="d-block w-100 img-fluid rounded" alt="<?= $item['Name'] ?>">
+                    </div>
+                <?php endforeach; ?>
             </div>
+
             <button class="carousel-control-prev" type="button" data-bs-target="#carouselExampleIndicators" data-bs-slide="prev">
                 <span class="carousel-control-prev-icon" aria-hidden="true"></span>
                 <span class="visually-hidden">Previous</span>
             </button>
+
             <button class="carousel-control-next" type="button" data-bs-target="#carouselExampleIndicators" data-bs-slide="next">
                 <span class="carousel-control-next-icon" aria-hidden="true"></span>
                 <span class="visually-hidden">Next</span>
             </button>
         </div>
     </div>
-    <!-- This is the scalable model for featured items in md and up
-        This will need to be looped with PHP to create items when we have DB set up. -->
+
+    <!-- This is the scalable model for featured items in md and up -->
     <div class="container-fluid" id="desktopCards">
-        <div class="row py-5 g-2">
-            <div class="card col-lg-3 col-md-4 col-sm-6 p-3">
-                <img src="./images/scotts_S.png" class="img-fluid rounded">
-                <div class="card-body">
-                    <h5 class="card-title text-center"></h5>
-                    <p class="card-text"></p>
+        <div class="row py-3 g-3">
+            <?php foreach ($perfectConditionItems as $item) : ?>
+                <div class="col-lg-3 col-md-4 col-sm-6">
+                    <div class="card h-100">
+                        <?php 
+                            $imgData = base64_encode($item['Img']);
+                            $imgSrc = "data:image/jpeg;base64,{$imgData}";
+                        ?>
+                        
+                        <a href="product.php?product_id=<?= $item['ItemId'] ?>" class="link-dark link-underline link-underline-opacity-0" >
+
+                            <img src="<?= $imgSrc ?>" class="img-fluid rounded card-img-top mx-auto d-block pb-1" alt="<?= $item['Name'] ?>" style="width: 17vw; height: 15vw;">
+
+                            <div class="card-body py-1">
+                                <h5 class="card-title text-center"><?= $item['Name'] ?></h5>
+                                <p class="card-text text-center pt-2">$<?= $item['Price'] ?></p>
+                            </div>
+                            
+                            <div class="card-footer py-0">
+                                <!-- <small class="text-muted"><?= $item['condition'] ?> condition</small><br> -->
+                                <small class="text-muted">Reserve now</small>
+                            </div>
+                        </a>
+                    </div>
                 </div>
-                <ul class="list-group list-group-flush rounded">
-                    <li class="list-group-item text-center"></li>
-                </ul>
-                <div class="card-body text-center rounded">
-                </div>
-            </div>
-            <div class="card col-lg-3 col-md-4 col-sm-6 p-3">
-                <img src="./images/scotts_S.png" class="img-fluid rounded">
-                <div class="card-body">
-                    <h5 class="card-title text-center"></h5>
-                    <p class="card-text"></p>
-                </div>
-                <ul class="list-group list-group-flush rounded">
-                    <li class="list-group-item text-center"></li>
-                </ul>
-                <div class="card-body text-center rounded">
-                </div>
-            </div>
-            <div class="card col-lg-3 col-md-4 col-sm-6 p-3">
-                <img src="./images/scotts_S.png" class="img-fluid rounded">
-                <div class="card-body">
-                    <h5 class="card-title text-center"></h5>
-                    <p class="card-text"></p>
-                </div>
-                <ul class="list-group list-group-flush rounded">
-                    <li class="list-group-item text-center"></li>
-                </ul>
-                <div class="card-body text-center rounded">
-                </div>
-            </div>
-            <div class="card col-lg-3 col-md-4 col-sm-6 p-3">
-                <img src="./images/scotts_S.png" class="img-fluid rounded">
-                <div class="card-body">
-                    <h5 class="card-title text-center"></h5>
-                    <p class="card-text"></p>
-                </div>
-                <ul class="list-group list-group-flush rounded">
-                    <li class="list-group-item text-center"></li>
-                </ul>
-                <div class="card-body text-center rounded">
-                </div>
-            </div>
+            <?php endforeach; ?>
         </div>
     </div>
 </div>
-<hr>
+<hr> 
 
 <!-- This is the about section to be included here on the homepage. The content needs to be created still -->
 <div class="MMblue border border-black border-5 container color-red mb-4">
