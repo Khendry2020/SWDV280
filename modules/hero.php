dd6c1c0c6299927b8bc18a01cb0b7c848b00f840
bradmj17 <bradmj17@gmail.com>
2023-11-29 14:17:08 -0700

    capitalize image path in hero.php for ubuntu use

--- modules/hero.php
+++ modules/hero.php
@@ -1,6 +1,6 @@
 <!-- ======= Hero Section =======  --> <!-- add    d-md-block d-none   to make it disappear on small -->
 <div id="hero" class="scotts-brown hero text-center">      
-  <img src="/swdv280/images/scottslogo.png" alt="" class="img-fluid">
+  <img src="/SWDV280/images/scottslogo.png" alt="" class="img-fluid">
 </div><!-- End Hero -->
 <!-- THIS IS EXPERIMENTAL and BROKEN
 <div id="hero" class="scotts-brown hero text-center d-md-none d-block">
