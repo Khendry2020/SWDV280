391d7900f5a301c1da23d3cfa175e64a805b957a
Brandon Swan <arcaneblunder@gmail.com>
2023-12-01 13:37:05 -0700

    Fixed admin errors pages.

--- admin/errors/db_error_connect.php
+++ admin/errors/db_error_connect.php
@@ -1,8 +1,9 @@
 <?php include( $_SERVER['DOCUMENT_ROOT'] . '/swdv280/modules/head.php'); ?>
-<?php include( $_SERVER['DOCUMENT_ROOT'] . '/swdv280/modules/header.php'); ?>
+<h4 class="text-center bg-dark text-light m-0 py-2">Administration</h4>
+<?php include( $_SERVER['DOCUMENT_ROOT'] . '/swdv280/admin/modules/hero.php'); ?>
+<?php include( $_SERVER['DOCUMENT_ROOT'] . '/swdv280/admin/modules/admin_bar.php'); ?>
 <section>
     <h1>Database Error</h1>
     <p>An error occurred connecting to the database.</p>
     <p class="last_paragraph">Error message: <?php echo $error_message; ?></p>
 </section>
-<?php include( $_SERVER['DOCUMENT_ROOT'] . '/swdv280/modules/footer.php'); ?>
