49efe2da171c3f5fe8cc7b2ecde6dc6d098f1f77
Cyril Soltau <cyrilsoltau@my.cwi.edu>
2023-11-30 20:32:56 -0700

    Added user validation to donationusers

--- admin/add/add_donationUsers.php
+++ admin/add/add_donationUsers.php
@@ -4,7 +4,10 @@
 session_start();
     include('./../model/database.php');
     include('./../model/donation_db.php');
-
+    if (!$_SESSION['isAdmin'] || $_SESSION['isAdmin'] == NULL || isset($_SESSION['adminLogError'])) {
+        $_SESSION['notification'] = 'Failed to log into. Please try again.';
+        header('Location: /swdv280/index.php');
+    }
     $name = '';
 $itemName = '';
 $phone = '';
@@ -24,12 +27,15 @@ $date = '';
         $phone = filter_input(INPUT_POST, 'phone');
         $email = filter_input(INPUT_POST, 'email');
         $date = filter_input(INPUT_POST, 'datepicker');
-
+        if($name == NULL|| $itemName == NULL || $phone == NULL || $email == NULL || $date == NULL) {            
+			$_SESSION['notification'] = 'Fildes can not be blank. Please try again.';
+		}else{
        add_donation($name,$itemName,$phone,$email, $date);
 			$_POST = [];
 			$_SESSION['notification'] = 'User added successfully.';
 			
 			header("Location: ../donations.php"); 
+        }
       
     }
 ?>
@@ -57,20 +63,23 @@ $date = '';
 
             <h3>Add Donated furniture</h3>
 
-            <form action="" method="post"  class="row gy-2 gx-3 align-items-center pt-4" >
+            <form action="" method="post"  enctype="multipart/form-data" class="row gy-2 gx-3 align-items-center pt-4" >
+            <span class="text-danger small ms-3"></span>
                 <div class="col-auto">
                     <label for="userName" class="form-label fw-bold">User's Name:</label>
                     <input type="text" class="form-control border border-3 rounded" id="userName" name="userName">
+               
                 </div>
 
                 <div class="col-auto">
                     <label for="itemName" class="form-label fw-bold">Item name:</label>
                     <input type="text" class="form-control border border-3 rounded" id="itemName" name="itemName">
+              
                 </div>
 
                 <div class="col-auto">
                     <label for="phone" class="form-label fw-bold">Phone:</label>
-                    <input type="text" class="form-control border border-3 rounded" id="phone" name="phone">
+                    <input type="text" placeholder="000-000-0000" class="form-control border border-3 rounded" id="phone" name="phone">
                 </div>
 
                 <div class="col-auto">
